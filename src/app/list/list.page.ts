55e2f1c5d90668377fb37df2df014f439b0bb7a9
DiegoSilvaxxx <DiegoSilvaxxx>
2019-07-26 03:22:02 -0300

    26/07/2019-Ferias

--- src/app/list/list.page.ts
+++ src/app/list/list.page.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { NavController } from '@ionic/angular';
+import { NavController, Platform } from '@ionic/angular';
 import { Router } from '@angular/router';
 import * as firebase from 'firebase';
 import { AngularFireAuth } from 'angularfire2/auth';
 import { Perfil } from '../model/perfil';
 import { Usuario } from '../model/usuario';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 
 @Component({
   selector: 'app-list',
@@ -13,7 +15,13 @@ import { Usuario } from '../model/usuario';
 })
 export class ListPage {
 
+  idUsuario: string;
+  usuarioEmail: string;
+
+  picture: string = "../../assets/imagens/1.gif";
+
   firestore = firebase.firestore();
+  settings = { timestampsInSnapshots: true }
 
   usuario: Usuario = new Usuario();
 
@@ -34,29 +42,55 @@ export class ListPage {
     'build'
   ];
   public items: Array<{ title: string; note: string; icon: string }> = [];
+
   constructor(public navctrl: NavController,
-    public router: Router,
-    private firebaseauth: AngularFireAuth) {
+    private platform: Platform,
+    private splashScreen: SplashScreen,
+    private statusBar: StatusBar,
+    private firebaseauth: AngularFireAuth,
+    public router: Router
+  ) {
 
-    this.firebaseauth.authState.subscribe(obj => {
-      this.id = this.firebaseauth.auth.currentUser.uid;
+    this.initializeApp();
+  }
 
+  initializeApp() {
 
-
+    this.platform.ready().then(() => {
+      this.statusBar.styleDefault();
+      this.splashScreen.hide();
     });
 
-    for (let i = 1; i < 11; i++) {
-      this.items.push({
-        title: 'Item ' + i,
-        note: 'This is item #' + i,
-        icon: this.icons[Math.floor(Math.random() * this.icons.length)]
-      });
-    }
-  }
+    this.firebaseauth.authState
+      .subscribe(obj => {
+
+        this.id = this.firebaseauth.auth.currentUser.uid;
+        this.idUsuario = this.firebaseauth.auth.currentUser.uid;
+        this.usuarioEmail = this.firebaseauth.auth.currentUser.email;
+        console.log(this.idUsuario)
+        this.downloadFoto();
+      }
+
+
+
+      )
+  };
+
 
   ngOnInit() {
     this.obterPerfil();
   }
+
+  downloadFoto() {
+    let ref = firebase.storage().ref()
+      .child(`perfil/${this.idUsuario}.jpg`);
+
+    ref.getDownloadURL().then(url => {
+      this.picture = url;
+    })
+  }
+
+
 
   obterPerfil() {
     var ref = firebase.firestore().collection("usuario").doc(this.id);
